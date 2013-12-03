da906ba83f8ed77b419d9ca385ca950db3f9f1ac
Yu Bin <bingle.yu@gmail.com>
2013-12-03 14:07:07 +0800

    : in setup_mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain:              => "binger.herokuapp.com",
+  :domain              => "binger.herokuapp.com",
   :user_name            => "bingle.yu@gmail.com",
   :password             => "foforwgqvkcegtuh",
   :authentication       => "plain",
