cd016221d1646ff037db375d8cc73a92c26b08fe
Yu Bin <bingle.yu@gmail.com>
2013-12-13 17:12:41 +0800

    panel hold h1

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Binger::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
-  resources :password_resets
+  resources :password_resets, only: [:new, :edit, :create, :update]
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
