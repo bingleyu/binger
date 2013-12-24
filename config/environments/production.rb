fd76c8687fa6e11b05f47a297d931201d6335710
Yu Bin <bingle.yu@gmail.com>
2013-12-24 17:12:27 +0800

    pro.rb true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Binger::Application.configure do
   config.force_ssl = true
   
   config.action_mailer.default_url_options = { :host => 'binger.herokuapp.com' }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
